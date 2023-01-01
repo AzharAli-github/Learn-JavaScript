f3391489f2a058653d52a466dd1c0d87e6298207
AzharAli-github <78659730+AzharAli-github@users.noreply.github.com>
2023-01-01 13:57:01 +0500

    Basic-del-1
    I am deleting the basic file from master branch

--- Basics/2.Arrays/js/script.js
+++
@@ -1,9 +0,0 @@
-// var colors = ['red', 'blue', 'yellow', 'green'];
-// for(var i=0; i<4; i++){
-//     console.log(colors[i])
-// }
-
-var colors = ['red', 'blue', 'yellow', 'green', 'orange', 'purple'];
-for(var i = 0; i < colors.length; i++){
-    console.log(colors[i]);
-}
