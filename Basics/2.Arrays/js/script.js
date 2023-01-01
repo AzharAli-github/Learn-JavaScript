46e6d51bd9bff18ff816b3670e0a8ba2966ecf46
Azhar Ali <azharborithic11@gmail.com>
2023-01-01 13:05:31 +0500

    arrays

---
+++ Basics/2.Arrays/js/script.js
@@ -0,0 +1,9 @@
+// var colors = ['red', 'blue', 'yellow', 'green'];
+// for(var i=0; i<4; i++){
+//     console.log(colors[i])
+// }
+
+var colors = ['red', 'blue', 'yellow', 'green', 'orange', 'purple'];
+for(var i = 0; i < colors.length; i++){
+    console.log(colors[i]);
+}
