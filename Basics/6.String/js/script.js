bc693c6a45453be05c99a21a7ee93b0e2c488e70
Azhar Ali <azharborithic11@gmail.com>
2023-01-04 00:18:30 +0500

    main

---
+++ Basics/6.String/js/script.js
@@ -0,0 +1,41 @@
+// var string = "Hello ";
+// string += "World";
+// console.log(string + "!");
+
+
+    //   Regular Math Operators
+
+// console.log((5 + 4) / 3);
+// console.log(undefined/5);
+// function test1 (a) {
+//     console.log(a / 5);
+// }
+// test1();
+
+
+
+        //    Equality
+// var x = 4, y = 4;
+// if(x == y)
+// {
+//     console.log("x=4 is equal to y=4");
+// }
+
+// x = "4";
+// if(x == y){
+//     console.log("x=4 is equal to y=4");
+// }
+
+
+
+            // Strickt Equality
+
+// var x = 4, y = 4;
+// if (x === y){
+//     console.log("Strict: x=4 is equal to y=4");
+// }
+// else{
+//     console.log("Strict: x=4 is NOT equal to y=4");
+// }
+
+
