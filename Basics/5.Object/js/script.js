6c0073725059a45b0712a2fce0f3516562729e29
Azhar Ali <azharborithic11@gmail.com>
2023-01-03 14:09:21 +0500

    Object

---
+++ Basics/5.Object/js/script.js
@@ -0,0 +1,25 @@
+// firstName: "Azhar",
+// lastName: : "Ali",
+// social: {
+//             linkedin : "azharali",
+//             twitter : "azharali",
+//             facebook : "courserawebDev"
+//         }
+
+
+
+var x;
+console.log(x);
+
+if(x == undefined){
+    console.log("x is undefined");
+}
+
+var x=5;
+console.log(x);
+if(x == undefined){
+    console.log("x is undefined")
+}
+else{
+    console.log("x has been defined")
+}
