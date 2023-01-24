c567483cdd0aef9c03c94c4c32f8ec439f37ac45
Azhar Ali <azharborithic11@gmail.com>
2023-01-24 12:55:13 +0500

    DOM

---
+++ JavaScript/Advance/DOM/1.Into & Methods/script.js
@@ -0,0 +1,19 @@
+let textOne = document.getElementById('textOne');
+
+function DisplayOne() {
+    textOne.innerHTML = `
+    <h1>HTML DOM</h1>
+    To under Understand DOM you must have the basic experiece in HTML, and CSS so then you can move on to JavaScript get the JS Basics Done and then move to JavaScript DOM. You will not understand What I have Done in this Program if you are a begginer. Check out my JS File I have explained all the codes with comments.<br>
+    <img src="images/DOM.gif" />
+    <br> The HTML DOM is a standard object model and programming interface for HTML. It defines:
+    <li>The HTML elements as objects</li>
+    <li>The properties of all HTML elements</li>
+    <li>The methods to access all HTML elements</li>
+    <li>The events for all HTML elements</li>
+    The W3C DOM standard is separated into 3 different parts:
+    <li>Core DOM - standard model for all document types</li>
+    <li>XML DOM - standard model for XML documents</li>
+    <li>HTML DOM - standard model for HTML documents
+    </li>
+    `
+}
