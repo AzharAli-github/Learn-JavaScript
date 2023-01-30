3fd3dc59a23dacd3ff34570a443082c141e498ca
Azhar Ali <azharborithic11@gmail.com>
2023-01-30 12:59:55 +0500

    DOM-Animation

---
+++ JavaScript/Advance/DOM/7. DOM Animation/script.js
@@ -0,0 +1,17 @@
+function Move() {
+    let id = null;
+    const elem = document.querySelector('.animate');
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
