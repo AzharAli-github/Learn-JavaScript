595b4fc9352df139d20a5bf121fd5ec05c7c4460
Azhar Ali <azharborithic11@gmail.com>
2023-02-03 02:40:02 +0500

    DOM-Root-Nodes

---
+++ JavaScript/Advance/DOM/11. DOM Root Nodes/js/script.js
@@ -0,0 +1,15 @@
+// Example One
+
+let textOne = document.getElementById('textOne');
+
+function DisplayOne() {
+    textOne.innerHTML = document.body.innerHTML;
+}
+
+// Example Two
+
+let textTwo = document.getElementById('textTwo');
+
+function DisplayTwo() {
+    textTwo.innerHTML = document.documentElement.innerHTML;
+}
